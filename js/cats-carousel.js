347b75d08448cbcd8c43e9186a44092f3dc0a08f
Ivan Pirija <ivanpirija@hotmail.com>
2021-07-12 15:56:50 +0200

    minor fixes to scss

--- js/cats-carousel.js
+++ js/cats-carousel.js
@@ -11,6 +11,24 @@ fetch("cats.json")
   .then((json) => {
     let catCarousel = document.getElementById("kitten-carousel");
     catCarousel.innerHTML = buildCarouselHTML(json.cats);
+
+    let kittenButtons = document.getElementsByClassName("udomi-carusel-button");
+
+    [...kittenButtons].forEach((kittenButton) => {
+      kittenButton.addEventListener("click", () => {
+        const domElement = kittenButton.parentNode.parentNode;
+        const carouselButtonId = kittenButton.dataset.carouselButtonId;
+        const catName = kittenButton.dataset.catName;
+
+        let result = confirm(`Zelite li udomiti ${catName}?`);
+
+        if (result) {
+          document.getElementById(carouselButtonId).remove();
+          domElement.remove();
+        }
+      });
+    });
+
     return catCarousel;
   })
 
@@ -129,13 +147,13 @@ function buildCarouselHTML(cats) {
             />
             <h2>${cat.name.toUpperCase()}</h2>
   
-            <span class="card-modal" data-modal-content>
+            <div class="card-modal" data-modal-content>
                 <h1>${cat.name}</h1>
                 <p>Color: ${cat.color}</p>
                 <P>Age: ${cat.age}</P>
-                <button class="udomi" data-modal="modal-one">UDOMI</button>
+                <button class="udomi-carusel-button" data-cat-name="${cat.name}" data-carousel-button-id="carousel-button-id-${cat.name}" data-modal="modal-one">UDOMI</button>
                 <button class="modal-close modal-exit">X</button>              
-            </span>
+            </div>
             </a>
           </div>
       `;
